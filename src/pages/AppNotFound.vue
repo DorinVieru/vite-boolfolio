d7a7f03ce81ab53c6f32f35e16e4727badcb3a13
DorinVieru <dorino.vieru@gmail.com>
2024-03-04 19:45:05 +0100

    [BONUS] Create a 404 page

---
+++ src/pages/AppNotFound.vue
@@ -0,0 +1,30 @@
+<!-- SEZIONE JS SCRIPT -->
+<script>
+export default {
+    name:'AppNotFound',
+}
+</script>
+
+<!-- SEZIONE HTML -->
+<template lang="">
+    <div>
+        <div class="container mt-5"> 
+            <div class="row"> 
+                <div class="col-12"> 
+                    <h1 class="text-center fw-bold">404 - Not Found</h1>
+                    <h4 class="text-center">Sembra che tu non stia trovando quello che stai cercando!</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<!-- SEZIONE STYLE -->
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as *;
+@use '../styles/generals.scss' as *;
+
+h1{
+    font-size: 80px;
+}
+</style>
